0f9222812af829f89f29c9f4916662f1064146c3
mprashant1100 <prashantmishra1100@gmail.com>
2026-01-20 18:29:20 +0530

    feat: logger file is created for all the INFO i will also show the timestamps

---
+++ logger.py
@@ -0,0 +1,20 @@
+import logging
+
+def get_logger(name="fintrace"):
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
